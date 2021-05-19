d5d56c3573e6d1ef3c9da6f6796f3922a6755eb3
Edoardo <edoardo.malfatto@hotmail.it>
2021-05-19 17:18:28 +0200

    agg

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('guest.home');
+    }
+}
